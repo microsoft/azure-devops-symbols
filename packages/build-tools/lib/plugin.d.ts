c1687a61db500dec244fb4ff0824c46155c79845
Danny van Velzen <dannyvv@microsoft.com>
2021-05-24 14:39:03 -0700

    Update .gitignore

--- packages/build-tools/lib/plugin.d.ts
+++
@@ -1,9 +0,0 @@
-import * as webpackTypes from "webpack";
-export interface AzureDevOpsSymbolsPluginOptions {
-    organization: string;
-}
-export declare class AzureDevOpsSymbolsPlugin {
-    organization: string | undefined;
-    constructor(options?: AzureDevOpsSymbolsPluginOptions);
-    apply(compiler: webpackTypes.Compiler): void;
-}
