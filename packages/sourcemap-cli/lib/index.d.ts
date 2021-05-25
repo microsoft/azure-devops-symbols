2e85bed509c36dd8171e26a0f7cccac2ab8b9acd
dannyvv <dannyvv@microsoft.com>
2021-05-24 20:18:51 -0700

    Update .gitignore (#5)
    * Update .gitignore
    
    * Change files
    
    * Change files
    
    * Bump Lage and BeachBall

--- packages/sourcemap-cli/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const sourceMapClientKeyField = "x_microsoft_symbol_client_key";
-export declare function computeSourceMapUrlLine(organization: string, clientKey: string, sourceFileName: string): string;
-export declare function setClientKeyOnSourceMap(clientKey: string, sourceMap: any): void;
-export declare function indexJsMapFileAsync(organization: string, hashAlgo: string, jsMapFile: string): Promise<void>;
