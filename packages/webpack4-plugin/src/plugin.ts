ccde00c7b552599ab9612c0cac24dc39e643c4a7
Yoni Feigelson <yfeigelson@microsoft.com>
2022-03-25 17:30:39 +0300

    Some tidying up

--- packages/webpack4-plugin/src/plugin.ts
+++ packages/webpack4-plugin/src/plugin.ts
@@ -1,4 +1,3 @@
-// Only use the static one for types, so that we can use the dynamic version of webpack we get from the plugin intialization
 import * as webpackTypes from "webpack";
 import * as webpackSources from "webpack-sources";
 import * as path from "path";
@@ -29,7 +28,11 @@ export class AzureDevOpsSymbolsPlugin {
   apply(compiler: webpackTypes.Compiler) {
     const options = compiler.options;
     // If we don't have source-map as a dev-tool this plugin doesn't need to do anything
-    if (!options.devtool || !options.devtool.includes("source-map")) {
+    if (
+      !options.devtool ||
+      options.devtool === true ||
+      !options.devtool.includes("source-map")
+    ) {
       console.log(
         `${pluginName}: returning since there's no source-map devtool`
       );
@@ -61,11 +64,14 @@ export class AzureDevOpsSymbolsPlugin {
       // should push the symbols.
       compilation.hooks.afterOptimizeAssets.tap(pluginName, (assets) => {
         for (const file of Object.keys(assets)) {
-          let asset = compilation.getAsset(file);
+          const asset = compilation.getAsset(file);
           if (asset) {
+            // @ts-ignore - this is correct for Webpack v4. There's a webpack-sources mismatch
             const sourceMap = asset.source.map(
               sourceMapOptions
             ) as webpackSources.SourceMapSource;
+
+            // This will only pass for assets
             if (sourceMap) {
               // Compute the hash of the sourcefile (before appending the sourceUrl comment)
               const hash = webpack.util.createHash(
@@ -87,7 +93,7 @@ export class AzureDevOpsSymbolsPlugin {
                 sourceMapFileName
               );
               const sourceWithMapLineAppended = new webpackSources.ConcatSource(
-                // @ts-ignore - this is correct for Webpack v4. There's a webpack-sources mismatch
+                // @ts-ignore is fine for Webpack v4...
                 asset.source.source(),
                 sourceMapLineToAppend
               );
@@ -95,36 +101,36 @@ export class AzureDevOpsSymbolsPlugin {
                 new webpackSources.SourceMapSource(
                   sourceWithMapLineAppended.source(),
                   asset.name,
-                  // @ts-ignore is fine for Webpack v4
-                  sourceMap, // Note: this seems to have no affect on the output .map file
+                  // @ts-ignore is fine for Webpack v4...
+                  sourceMap, // Note: For w/e reason this has no affect on the output .map file
                   undefined,
                   undefined,
                   true
                 );
-              compilation.updateAsset(
-                asset.name,
-                // @ts-ignore is fine for Webpack v4
-                assetWithClientKeyOnSourceMap,
-                undefined
-              );
 
-              // Update the sourcemap with the metadata
-              // No idea why updating the source map associated with the actual .js file has no effect - I'd think it would
               const sourceMapAssetName = file + ".map";
               const actualSourceMapAsset =
                 compilation.getAsset(sourceMapAssetName);
-              const actualSource = JSON.parse(
-                // @ts-ignore - this is correct for Webpack v4
-                actualSourceMapAsset.source.source()
-              );
-              if (actualSourceMapAsset) {
-                // Add the sourcemap client id field to the sourcemap json object.
-                setClientKeyOnSourceMap(clientKey, actualSource);
+              if (actualSourceMapAsset) {                
+                // Update the asset (the .js file)
+                compilation.updateAsset(
+                  asset.name,
+                  // @ts-ignore correct for Webpack v4...
+                  assetWithClientKeyOnSourceMap,
+                  undefined
+                );
+
+                // Add the sourcemap client id field to the sourcemap json object, and update the sourcemap
+                const sourceMapData = JSON.parse(
+                  // @ts-ignore - correct for Webpack v4...
+                  actualSourceMapAsset.source.source()
+                );
+                setClientKeyOnSourceMap(clientKey, sourceMapData);
 
                 compilation.updateAsset(
                   sourceMapAssetName,
-                  // @ts-ignore - this is correct for Webpack v4
-                  new webpackSources.RawSource(JSON.stringify(actualSource))
+                  // @ts-ignore - correct for Webpack v4...
+                  new webpackSources.RawSource(JSON.stringify(sourceMapData))
                 );
               }
             }
